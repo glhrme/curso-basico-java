e854ec1502c97fdaf2a4303569b9746067e487e2
Guilherme dos Santos <guilherme@guisantos.com.br>
2021-04-10 19:07:18 -0300

    testando valores

---
+++ src/br/com/guisantos/Main/TestaValores.java
@@ -0,0 +1,13 @@
+package br.com.guisantos.Main;
+
+
+public class TestaValores {
+    public static void main(String[] args) {
+        int primeiro = 5;
+        int segundo = 7;
+        segundo = primeiro;
+        primeiro = 10;
+
+        System.out.println(primeiro + "/" + segundo);
+    }
+}
