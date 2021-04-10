c065b20a5af8feffd001f2e520bf637068e1f768
Guilherme dos Santos <guilherme@guisantos.com.br>
2021-04-10 18:55:19 -0300

    caracteres char

---
+++ src/br/com/guisantos/Main/Caracteres.java
@@ -0,0 +1,10 @@
+package br.com.guisantos.Main;
+
+public class Caracteres {
+    public static void main(String[] args) {
+        char letra = 'a';
+        char letraNumero = 64;
+        System.out.println(letra);
+        System.out.println(letraNumero);
+    }
+}
