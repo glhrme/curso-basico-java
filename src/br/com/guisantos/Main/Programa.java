5af59277eeda3bdc82365df81b3c4838608b86bb
Guilherme dos Santos <guilherme@guisantos.com.br>
2021-04-10 18:43:02 -0300

    finaliza trabalho com variaveis inteiras e double

--- src/br/com/guisantos/Main/Programa.java
+++ src/br/com/guisantos/Main/Programa.java
@@ -3,5 +3,25 @@ package br.com.guisantos.Main;
 public class Programa {
 	public static void main(String[] args) {
 		System.out.println("Oi Mundo");
+		int idade;
+		idade = 21;
+		double salario = 1000.80;
+		System.out.println("Minha idade é: " + idade + " anos");
+		System.out.println("Meu salário é: R$ " + salario);
+
+		/**
+		 * Exemplos de Divisão
+		 * **/
+
+		int divisao = 5 / 2;
+		System.out.println(divisao);
+
+		double segundaTentativaDeDivisao = 5 / 2;
+		System.out.println(segundaTentativaDeDivisao);
+
+		double terceiraTentativaDeDivisao = 5 / 2.0;
+		System.out.println(terceiraTentativaDeDivisao);
+
+		/** Java é mt chato, para um resoltado ser calcular como float, pelo menos um valor deve ser float **/
 	}
 }
