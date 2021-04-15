284d541c139c8fb85279a030731b0d8d741f64bc
Guilherme dos Santos <guilherme@guisantos.com.br>
2021-04-14 23:36:16 -0300

    Inicia aula de arrays

--- src/br/com/bytebank/banco/test/Teste.java
+++ src/br/com/bytebank/banco/test/Teste.java
@@ -1,33 +1,16 @@
 package br.com.bytebank.banco.test;
 
-import br.com.bytebank.banco.modelo.Cliente;
-import br.com.bytebank.banco.modelo.Conta;
-import br.com.bytebank.banco.modelo.ContaCorrente;
-import br.com.bytebank.banco.modelo.ContaPoupanca;
-
 public class Teste {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
+        int[] idades = new int[5];
+        for(int i = 0; i < idades.length; i++){
+            idades[i] = 5+i*9;
+        }
 
-//		System.out.println("x");
-//		System.out.println(3);
-//		System.out.println(false);
-		
-		Object cc = new ContaCorrente(22, 33);
-		Object cp = new ContaPoupanca(33, 22);
-		Object cliente = new Cliente();
-		
-		System.out.println(cc);
-		System.out.println(cp);
-		
-		//println(cliente);
-	}
-	
-	static void println() {}
-	static void println(int a) {}
-	static void println(boolean valor) {}
-	
-	static void println(Object referencia) {
-	}
-
+        for(int i = 0; i < idades.length; i++){
+            System.out.println(idades[i]);
+        }
+        
+    }
 }
